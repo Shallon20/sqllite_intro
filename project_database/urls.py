fe20a5acfd5f52a23d84cd3d68adfe0360302c47
Shallon <shallonmaria2001@gmail.com>
2024-11-17 23:07:12 +0200

    Adding deposits to customers

--- project_database/urls.py
+++ project_database/urls.py
@@ -25,6 +25,7 @@ path('add/customer', views.add_customer, name='add_customer'),
     path('customers/delete/<int:customer_id>', views.delete_customer, name='delete_customer'),
     path('customers/update/<int:customer_id>', views.update_customer, name='update_customer'),
     path('customers/search', views.search_customer, name='search_customer'),
+    path('customers/deposit/<int:customer_id>', views.deposit, name='deposit'),
     path('customers/details/<int:customer_id>', views.customer_details, name='customer_details'),
     path('admin/', admin.site.urls),
 ]
--- sacco/app_forms.py
+++ sacco/app_forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from sacco.models import Customer
+from sacco.models import Customer, Deposit
 
 GENDER_CHOICES = {'Male': 'Male', 'Female': 'Female'}
 
@@ -12,4 +12,12 @@ class CustomerForm(forms.ModelForm):
         widgets = {
             'dob': forms.DateInput(attrs={'type': 'date', 'min': '1980-01-01', 'max': '2005-12-31'}),
             'weight': forms.NumberInput(attrs={'type': 'number', 'min': '35', 'max': '100'}),
+        }
+
+class DepositForm(forms.ModelForm):
+    class Meta:
+        model = Deposit
+        fields = ['amount']
+        widgets = {
+            'amount': forms.NumberInput(attrs={'type': 'number', 'min': '0', 'max': '100000'}),
         }
